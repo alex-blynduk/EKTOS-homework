90d5f968016c49ff6757c9fc6e564dc5eb4b41bf
alex_blynduk <alexblynduk05@gmail.com>
2026-03-30 17:58:33 +0300

    Completed homework 1

---
+++ Homework1/int_to_binary/int_to_binary.c
@@ -0,0 +1,60 @@
+#include "int_to_binary.h"
+#include <stdio.h>
+
+//В мене не працює %b, хоча використовую С23, тому брав функції з інтернету
+
+void print_binary16(const int16_t n) {
+    const int bits = 16;
+    const unsigned int u = (unsigned int)n;
+
+    for (int i = bits - 1; i >= 0; i--) {
+        const unsigned int mask = 1 << i;
+        if (u & mask) {
+            printf("1");
+        } else {
+            printf("0");
+        }
+
+        if (0 == i % 8) {
+            printf(" ");
+        }
+    }
+    printf("\n");
+}
+
+void print_binary8(const int8_t n) {
+    const int bits = 8;
+    const unsigned int u = (unsigned int)n;
+
+    for (int i = bits - 1; i >= 0; i--) {
+        const unsigned int mask = 1 << i;
+        if (u & mask) {
+            printf("1");
+        } else {
+            printf("0");
+        }
+
+        if (0 == i % 8) {
+            printf(" ");
+        }
+    }
+    printf("\n");
+}
+
+void print_binary8u(const uint8_t n) {
+    const int bits = 8;
+
+    for (int i = bits - 1; i >= 0; i--) {
+        const unsigned int mask = 1 << i;
+        if (n & mask) {
+            printf("1");
+        } else {
+            printf("0");
+        }
+
+        if (0 == i % 8) {
+            printf(" ");
+        }
+    }
+    printf("\n");
+}
