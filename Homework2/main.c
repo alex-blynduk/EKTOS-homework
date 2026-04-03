c691c8a14ab6cd33447bf8c6422f60d510a14729
alex_blynduk <alexblynduk05@gmail.com>
2026-04-03 10:40:01 +0300

    Added homework2

---
+++ Homework2/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+bool unreachableFunction() {
+    printf("This text will not be displayed\n");
+    return true;
+}
+
+int main(void) {
+    //===TASK1====
+    if (true || unreachableFunction()) {
+        printf("Only this text will be displayed\n");
+    }
+
+    if (false && unreachableFunction()) {
+        //The function will also not be executed
+    }
+
+
+    //===TASK2====
+    if (2 & 5 ) {
+        printf("This code will not be reached, though both numbers considered true");
+    }
+
+    int num = 4;
+    num = num && ~(1 << 2); //we wanted to reset 2nd bit, but used wrong operator instead
+    if (num) {
+        printf("This text should not be displayed, but it will be because of the usage of wrong operator");
+    }
+
+    return 0;
+}
