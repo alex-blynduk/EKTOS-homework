acc43034f6eb93bfb5935ef0a05a8734e502f602
alex_blynduk <alexblynduk05@gmail.com>
2026-04-06 22:34:27 +0300

    Completed homework3

---
+++ Homework3/weak_func/weak_func.h
@@ -0,0 +1,8 @@
+#ifndef HOMEWORK3_WEAK_FUNC_H
+#define HOMEWORK3_WEAK_FUNC_H
+
+__attribute__((weak)) void weak_func1(void);
+// __attribute__((weak)) void weak_func2(void);
+__attribute__((weak)) void weak_func3(void);
+
+#endif //HOMEWORK3_WEAK_FUNC_H
